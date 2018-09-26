3c40ddc78ad2ab2d935b8f1c124db2f2de4e4148
Park-Ji-Hoon <30282681+ggoowlgns@users.noreply.github.com>
2018-09-26 21:20:22 +0900

    add

--- app/src/main/java/com/example/user/jj_ras_app/MainActivity.java
+++ app/src/main/java/com/example/user/jj_ras_app/MainActivity.java
@@ -1,62 +1,122 @@
 package com.example.user.jj_ras_app;
 
-import android.content.ContentValues;
+import android.content.Context;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+import com.example.user.jj_ras_app.Network.HttpClient;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView tv_outPut;
-    private String url = "https://220.67.124.128:8080";
+    BackgroundTask task;
+    String address, result;
+    private TextView textView;
+    private String url = "http://220.67.124.128:8080";
     private String res = "";
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // 위젯에 대한 참조
-        tv_outPut = (TextView) findViewById(R.id.tv_outPut);
+        textView = (TextView) findViewById(R.id.textView);
+        btn = (Button) findViewById(R.id.button);
 
-        //AsyncTask를 통해 HttpURLConnection 수행
-        NetworkTask networkTask = new NetworkTask();
-        networkTask.execute();
-    }
-
-    public class NetworkTask extends AsyncTask<Void, Void, Void> {
-        @Override
-        protected void onPreExecute(){
-            super.onPreExecute();
-        }
-
-
-        @Override
-        protected Void doInBackground(Void... params){
-            try {
-                Document doc = Jsoup.connect(url).get();
-                Elements tables = doc.select("table.table.table-striped");
-                System.out.println("----------------");
-                for(Element e: tables){
-                    res += e.text().trim();
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                task = new BackgroundTask(MainActivity.this);
+                try {
+                    String text = task.execute().get();
+                    Log.e("Detail2", text );
+                    textView.setText(text);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
                 }
-            } catch (Exception e){
-                e.printStackTrace();
             }
-            //RequestHttpURLConnection requestHttpURLConnection = new RequestHttpURLConnection();
-            //result = requestHttpURLConnection.request(url, values);
-            return null;
+        });
+    }
+
+    class BackgroundTask extends AsyncTask<Map<String, String>, Integer, String> {
+        String body = "";
+        Context context;
+        public BackgroundTask(Context context) {
+            this.context = context;
         }
 
         @Override
-        protected void onPostExecute(Void result){
-            tv_outPut.setText(res);
+        protected String doInBackground(Map<String, String>... maps) {
+            // Http 요청 준비 작업
+            HttpClient.Builder http = new HttpClient.Builder("POST", url+"/product/get_product");
+            //Http 요청 전송
+
+
+            HttpClient post = http.create();
+            post.request();
+
+// 응답 상태코드 가져오기
+            int statusCode = post.getHttpStatusCode();
+
+// 응답 본문 가져오기
+            body = post.getBody();
+
+            return body;
         }
+
+        protected void onPreExecute() {
+            Log.e("Detail",body);
+        }
+
+
     }
+
+    private String request(String urlStr) {
+        StringBuilder output = new StringBuilder();
+        try {
+            URL url = new URL(urlStr);
+            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+            if (conn != null) {
+                conn.setConnectTimeout(10000);
+                conn.setRequestMethod("POST");
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+
+                int resCode = conn.getResponseCode();
+                if (resCode == HttpURLConnection.HTTP_OK) {
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream())) ;
+                    String line = null;
+                    while(true) {
+                        line = reader.readLine();
+                        if (line == null) {
+                            break;
+                        }
+                        output.append(line + "\n");
+                    }
+
+                    reader.close();
+                    conn.disconnect();
+                }
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        return output.toString();
+    }
+
 }
